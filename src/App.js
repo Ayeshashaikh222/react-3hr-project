fb73c6adaaa37b0620109c5cfd88a2dd83ace274
Ayeshashaikh222 <ayeshashaikh0222@gmail.com>
2023-11-29 19:45:02 +0530

    added styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React, { useState } from 'react';
-// import './App.css';
+import React, {useState} from 'react';
+import './App.css';
 import AddUser from './components/Users/AddUser';
 import UserList from './components/Users/UserList';
 
 
 function App() {
   
-     const [userList, setUserList] = useState([]);
+     const [usersList, setUsersList] = useState([]);
 
     const addUserHandler = (uName, uAge) => {
-      setUserList((prevUserList) => {
-        return [...prevUserList, {name: uName, age: uAge}];
-      })
-    }
+      setUsersList((prevUsersList) => {
+
+        return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString()}];
+      });
+    };
+    
+
   return (
     <div>
       <AddUser onAddUser={addUserHandler}/>
-      <UserList users={userList} />
+      <UserList users={usersList} />
     </div>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,55 +2,67 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
-import UserList from './UserList';
-
+import ErrorModel from '../UI/ErrorModel';
 
 
 
 const AddUser = (props) => {
+
     const [entereduserName, setEnteredUserName] = useState('');
-    const [eneterduserAge, setEnteredUserAge] = useState('');
+    const [entereduserAge, setEnteredUserAge] = useState('');
+    const [error, setError] = useState();
+
 
     const formSubmitHandler = (event) => {
         event.preventDefault();
-        if (entereduserName.trim().length === 0 || eneterduserAge.trim().length === 0) {
+        if (entereduserName.trim().length === 0 || entereduserAge.trim().length === 0) {
+            setError({
+                title: 'Inavlid Input',
+                message: 'Please enter a valid name and age (non-empty values).'
+            });
             return;
         }
-        if (+eneterduserAge < 1) {
-            alert("please enter the valid age!")
+        if (+entereduserAge < 1) {
+            setError({
+                title: 'Inavlid Age',
+                message: 'Please enter a valid n age (grater then zero).'
+            });
+            return;
         }
-        // console.log(entereduserName, eneterduserAge);
-        props.onAddUser(entereduserName, eneterduserAge);
+        console.log(entereduserName, entereduserAge);
+        props.onAddUser(entereduserName, entereduserAge);
         setEnteredUserName('');
         setEnteredUserAge('');
 
-    }
+    };
 
     const OnUserInputHandler = (event) => {
         setEnteredUserName(event.target.value);
-
-
-    }
+    };
 
     const OnUserAgeHandler = (event) => {
         setEnteredUserAge(event.target.value);
+    };
 
-
-    }
+    const errorHandler = () => {
+        setError(null);
+    };
 
 
 
     return (
-        <Card className={classes.input}>
-            <form onSubmit={formSubmitHandler}>
-                <label htmlFor='username'>Username:</label>
-                <input id='username' value={entereduserName} type='text' onChange={OnUserInputHandler} />
-                <label htmlFor='age'>Age(years):</label>
-                <input id='age' type='number' value={eneterduserAge} onChange={OnUserAgeHandler} />
-                <Button type='submit'>Add User</Button>
-            </form>
-
-        </Card>
+        <div>
+            {error && <ErrorModel title={error.title} message={error.message} onConfirm={errorHandler} />}
+            <Card className={classes.input}>
+                <form onSubmit={formSubmitHandler}>
+                    <label htmlFor='username'>Username:</label>
+                    <input id='username' value={entereduserName} type='text' onChange={OnUserInputHandler} />
+                    <label htmlFor='age'>Age(years):</label>
+                    <input id='age' type='number' value={entereduserAge} onChange={OnUserAgeHandler} />
+                    <Button type='submit'>Add User</Button>
+                </form>
+            </Card>
+        </div>
     )
 
 }
